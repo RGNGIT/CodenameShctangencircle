f8ff25a1bc5d66074d4f9274f19c7587f5253d51
Exem <chernyakov.ghost@mail.ru>
2021-10-19 17:05:29 +0500

    vseNorm

--- CodenameShctangencircle/CodenameShctangencircle/Main.Designer.cs
+++ CodenameShctangencircle/CodenameShctangencircle/Main.Designer.cs
@@ -451,7 +451,7 @@ namespace CodenameShctangencircle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(627, 189);
+            this.ClientSize = new System.Drawing.Size(627, 187);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
--- CodenameShctangencircle/CodenameShctangencircle/ServerWorks.cs
+++ CodenameShctangencircle/CodenameShctangencircle/ServerWorks.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using ShctangenNetLib;
 
